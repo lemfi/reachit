67ecd0d24fa7c0f89d044ba4152fd1bda72bb513
lemfi <lemfi.22@gmail.com>
2021-08-30 23:12:26 +0200

    documentation

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -21,8 +21,6 @@ tasks.withType<Jar> {
     }
 
     duplicatesStrategy = DuplicatesStrategy.EXCLUDE
-//    from(sourceSets.main.get().output)
-//    dependsOn(configurations.runtimeClasspath)
     from({
         configurations.runtimeClasspath.get().filter { it.name.endsWith("jar") }.map { zipTree(it) }
     })
